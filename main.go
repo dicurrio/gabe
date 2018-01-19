55c837a49be33a61b64f9fe8a9646cd1aca24aea
Bob Chen <bobbychen@college.harvard.edu>
2018-01-19 12:00:57 -0700

    somewhat working tls

--- main.go
+++ main.go
@@ -12,25 +12,45 @@ import (
 	"syscall"
 	"time"
 
+	"google.golang.org/grpc/credentials"
+
 	pb "github.com/dicurrio/protorepo/fred"
 	"google.golang.org/grpc"
 )
 
-var (
-	hostAddress = os.Getenv("HOST_ADDRESS")
-	fredAddress = os.Getenv("FRED_ADDRESS")
+const (
+	fredCert    = "./tls/fred-cert.pem"
+	hostAddress = "localhost:3000"
+	fredAddress = "localhost:3001"
 )
+
+// var (
+// 	hostAddress = os.Getenv("HOST_ADDRESS")
+// 	fredAddress = os.Getenv("FRED_ADDRESS")
+// )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("%v %v %v", r.Proto, r.Method, r.URL.EscapedPath())
 	name := "Friend"
 
-	conn, err := grpc.Dial(fredAddress, grpc.WithInsecure())
+	// Create TLS credentials
+	creds, err := credentials.NewClientTLSFromFile(fredCert, "")
+	if err != nil {
+		log.Printf("Failed to load TLS files: %v", err)
+		fmt.Fprintf(w, "Internal Server Error")
+		return
+	}
+
+	// Establish gRPC connection
+	conn, err := grpc.Dial(fredAddress, grpc.WithTransportCredentials(creds))
 	if err != nil {
 		log.Printf("Failed to dial Fred: %v", err)
+		fmt.Fprintf(w, "Internal Server Error")
+		return
 	}
 	defer conn.Close()
 
+	// Send request
 	fredClient := pb.NewFredClient(conn)
 	req := &pb.Request{
 		Name: name,
@@ -39,9 +59,11 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Printf("Fred GetIndex error: %v", err)
 		fmt.Fprintf(w, "Internal Server Error")
-	} else {
-		fmt.Fprintf(w, res.GetMessage())
+		return
 	}
+
+	// Write Response
+	fmt.Fprintf(w, res.GetMessage())
 }
 
 func main() {
